ae831ec69c0d8f1a138b98391ac639a6d28207cf
nginnever <ginneversource@gmail.com>
2016-07-07 16:03:21 -0700

    init registry contract

---
+++ contracts/Registry.sol
@@ -0,0 +1,29 @@
+contract Registry {
+    mapping (bytes32 => address) public owners;
+    mapping (bytes32 => bytes) public registry;
+
+    modifier isOwner(bytes32 name)
+    {
+        if (msg.sender != owners[name]) throw;
+        _
+    }
+  
+    modifier isInit(bytes32 name)
+    {
+        if (owners[name] != 0) throw;
+        _
+    }
+  
+    function publish (bytes32 name, bytes hash) 
+        isOwner(name) 
+    {
+        registry[name] = hash;
+    }
+  
+    function init (bytes32 name, bytes hash)
+        isInit(name)
+    {
+        owners[name] = msg.sender;
+        registry[name] = hash;
+    }
+}
